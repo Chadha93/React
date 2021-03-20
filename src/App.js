c0d18fe6a180cc37bdbc04b823bcf4e23fffa101
Gaurav Chadha <65453826+Chadha93@users.noreply.github.com>
2021-03-20 22:44:33 +0530

    learning

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"; 
+ 
+function App() {   
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const sayHello = () => {
+    console.log("Say Hello")
+  }
+  return(
+     <div>                         {/*this is not actually HTML its the JSX*/} 
+      <h1>Hello World React</h1>
+      <button onClick={sayHello}></button>
     </div>
   );
 }
